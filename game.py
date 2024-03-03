86ba6cb954e4b97230c00bba2739f46b41710095
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2024-03-03 02:16:36 +0000

    Second commit

--- game.py
+++ game.py
@@ -19,7 +19,10 @@ def line_line_intersection(p1, p2, p3, p4):
     x2, y2 = p2
     x3, y3 = p3
     x4, y4 = p4
-    return ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)) != 0
+    denom = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+    t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
+    u = ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denom
+    return 0 <= t <= 1 and 0 <= u <= 1
 
 
 corners = []
@@ -45,7 +48,7 @@ while running:
                             is_shape_finished = True
                     elif not is_shape_finished:
                         has_overlap = False
-                        for i in range(len(corners) - 4):
+                        for i in range(len(corners) - 1):
                             if line_line_intersection(corners[-1], pos, corners[i], corners[i + 1]):
                                 has_overlap = True
                                 break
